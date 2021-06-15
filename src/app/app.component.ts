a0ee866056e0e1a7b9ef7a1fd0ed12728991b0a0
Shamant Vijayakumar Gudigenavar --CNTR <ShamantVijayakumar_Gudigenavar@contractor.amat.com>
2021-06-15 12:11:35 +0530

    ngx module demo code added

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'demo-ngx-bootstarp-exp';
+
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) { }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  ngOnInit(): void {
+  }
+}
